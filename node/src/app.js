47e8ded95433c2795b93edfa942e15d88421fdbc
Francesco <firefelix@gmail.com>
2015-10-21 01:25:35 +0200

    move node process files to /node

--- node/src/app.js
+++ node/src/app.js
@@ -2,9 +2,20 @@ import _spotify from 'spotify-node-applescript';
 import request from 'request-promise';
 import readlineSync from 'readline-sync';
 import { Parse } from 'node-parse-api';
-import { filter, sortBy, includes } from 'lodash';
+import { filter, sortBy, includes, range, debounce } from 'lodash';
 import keypress from 'keypress';
+import getCursorPosition from '../cursor-position';
 keypress(process.stdin);
+
+
+
+getCursorPosition(function(error, init) {
+  setInterval(function() {
+      time++;
+      _logInline(init.row, 'alpha-' + time);
+      _logInline(init.row + 1, 'bravo-' + time * time);
+  }, delay);
+});
 
 const APP_ID = 'f9wu4ZdFIMfE7CNlVKs9zj98Q9lOi0EPwB6tr0Fl',
   MASTER_KEY = 'NqcjMzG5vDZ0c2wz3k6K2RizTxLSoLCWTIv5o1a1',
@@ -19,13 +30,45 @@ function isMediaKey(key) {
   return includes([PLAY_PAUSE, PREV, NEXT], key);
 };
 
+function log(l) {
+  process.stdout.clearLine();
+  console.log(l);
+};
+
+function logInfos() {
+  ping = new Date();
+  currentSong = {title: 'BEAUTIFUL SONG'};
+  queue = range(10);
+  const hours = ('0' + ping.getHours()).slice(-2);
+  const minutes = ('0' + ping.getMinutes()).slice(-2);
+  const seconds = ('0' + ping.getSeconds()).slice(-2);
+  process.stdout.write(`
+    Now Playing: ${currentSong.title}\r\n
+    Songs in Queue: ${queue.length}\r\n
+    PING: ${hours}:${minutes}:${seconds}\r
+  `);
+  // log(`Now Playing: ${currentSong.title}`);
+  // cleanLine();
+  // process.stdout.write(`PING: ${hours}:${minutes}:${seconds}\r`);
+};
+
 let queue,
   interval,
   pointer,
   code,
   currentSong,
   insidePlayingNext,
+  ping,
   mediaInputs = [];
+
+function cursorInterface() {
+  return {
+    getCursorPosition: () => new Promise((resolve) => {
+      getCursorPosition((err, res) => resolve(err || res));
+    })
+  };
+};
+
 
 function rlinterface() {
   return {
@@ -78,23 +121,26 @@ const rl = rlinterface(),
 async function updateQueue() {
   const res = await app.find('Song', {where: {party_session: pointer, played: false}, limit: 1000});
   queue = res.results;
-  console.log(`SONGS IN QUEUE: ${queue.length}`);
   queue.sort((a, b) => (b.up_votes - b.down_votes) - (a.up_votes - a.down_votes));
 };
 
 async function playNext() {
-  // console.log('playNext');
+  // log('playNext');
   insidePlayingNext = true;
   await updateQueue();
   currentSong = queue.shift();
-  console.log(`Now Playing: ${currentSong.title}`);
   await app.update('Song', currentSong.objectId, { played: true });
   await spotify.playTrack(currentSong.href);
   insidePlayingNext = false;
 };
 
 async function controlSongState() {
-  // console.log('controlSongState');
+  const now = new Date();
+  const hours = ('0' + now.getHours()).slice(-2);
+  const minutes = ('0' + now.getMinutes()).slice(-2);
+  const seconds = ('0' + now.getSeconds()).slice(-2);
+  cleanLine();
+  process.stdout.write(`PING: ${hours}:${minutes}:${seconds}\r`);
   if (!insidePlayingNext) {
     const state = await spotify.getState();
     const track = await spotify.getTrack();
@@ -105,31 +151,32 @@ async function controlSongState() {
   }
 };
 
-// async function askForActions(prompt) {
-//   console.log(prompt);
-//   process.stdin.on('keypress', function (chunk, key) {
-//     if (key && key.ctrl && key.name == 'c') {
-//       process.exit(1);
-//     }
-//     switch (key.name) {
-//       case PLAY_PAUSE:
-//         console.log('PLAY_PAUSE');
-//         spotify.playPause();
-//         break;
-//       case NEXT:
-//         console.log('NEXT');
-//         if (!insidePlayingNext) {
-//           playNext();
-//         } else {
-//           console.log('discarded');
-//         }
-//         break;
-//     }
-//     resetInterval();
-//   });
-//   process.stdin.setRawMode(true);
-//   process.stdin.resume();
-// };
+async function askForActions(prompt) {
+  log(prompt);
+  process.stdin.on('keypress', function (chunk, key) {
+    if (key && key.ctrl && key.name == 'c') {
+      process.exit(1);
+      return;
+    }
+    switch (key.name) {
+      case PLAY_PAUSE:
+        log('PLAY_PAUSE');
+        spotify.playPause();
+        break;
+      case NEXT:
+        log('NEXT');
+        if (!insidePlayingNext) {
+          playNext();
+        } else {
+          log('discarded');
+        }
+        break;
+    }
+    // resetInterval();
+  });
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+};
 
 // function resetInterval() {
 //   if (interval) {
@@ -138,14 +185,14 @@ async function controlSongState() {
 //   interval = setInterval(controlSongState, 1000);
 // };
 
-async function controlMediaActions() {
-  const action = (await app.find('Action', {where: {party_session: pointer, active: true}})).results[0];
-  switch (action.type) {
-    case 'next_song':
-      const x = await app.update('Action', action.objectId, {active: false});
-      playNext();
-  }
-}
+// async function controlMediaActions() {
+//   const action = (await app.find('Action', {where: {party_session: pointer, active: true}})).results[0];
+//   switch (action.type) {
+//     case 'next_song':
+//       const x = await app.update('Action', action.objectId, {active: false});
+//       playNext();
+//   }
+// }
 
 async function runApp() {
   // const code = await rl.question(`Ahoy 'n welcome to Partify.\nPlease, scribe ye parrrty code:`);
@@ -155,10 +202,10 @@ async function runApp() {
     className: 'PartySession',
     objectId: code
   };
-  playNext();
-  setInterval(controlSongState, 1000);
-  setInterval(controlMediaActions, 1000);
+  // playNext();
+  // setInterval(controlSongState, 1000);
   // askForActions(`\nuse mediakeys to control songs. type 'CTRL + C' to quit`);
+  setInterval(logInfos, 1000);
 };
 
 runApp();
