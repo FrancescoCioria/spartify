dd353c6b2ba5cefbe907c1690c715122564dd59d
Francesco <firefelix@gmail.com>
2015-10-23 00:04:13 +0200

    save song only if not already in playlist

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -28,3 +28,31 @@ Parse.Cloud.define('removeUpvote', function(request, response) {
     }
   });
 });
+
+Parse.Cloud.beforeSave('Song', function(request, response) {
+  var query = new Parse.Query('Song');
+  query.equalTo('href', request.object.get('href'));
+  query.equalTo('party_session', request.object.get('party_session'));
+  query.find({
+    success: function(res) {
+      if (res.length === 0) {
+        var defaults = {
+          up_votes: 1,
+          down_votes: 0,
+          played: false
+        };
+        Object.keys(defaults).forEach(function(key) {
+          if (typeof request.object.get(key) === 'undefined') {
+            request.object.set(key, defaults[key]);
+          }
+        });
+        response.success();
+      } else {
+        response.error('song already in playlist');
+      }
+    },
+    error: function() {
+      response.error('song lookup failed');
+    }
+  });
+});
