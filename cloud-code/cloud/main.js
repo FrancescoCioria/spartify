e362f1e0b6a03f5c5e85094e5de6d68a92e1dbef
Francesco <firefelix@gmail.com>
2015-10-23 21:06:52 +0200

    beforeSave: if object has an id it's an update -> allow request

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -6,7 +6,7 @@ Parse.Cloud.define('addUpvote', function(request, response) {
       var upVotes = song.attributes.up_votes + 1;
       song.set('up_votes', upVotes);
       song.save(null);
-      response.success(upVotes);
+      response.success();
     },
     error: function() {
       response.error('song lookup failed');
@@ -21,7 +21,7 @@ Parse.Cloud.define('removeUpvote', function(request, response) {
       var upVotes = song.attributes.up_votes > 0 ? song.attributes.up_votes - 1 : 0;
       song.set('up_votes', upVotes);
       song.save(null);
-      response.success(upVotes);
+      response.success();
     },
     error: function() {
       response.error('song lookup failed');
@@ -29,30 +29,49 @@ Parse.Cloud.define('removeUpvote', function(request, response) {
   });
 });
 
-Parse.Cloud.beforeSave('Song', function(request, response) {
+Parse.Cloud.define('setAsPlayed', function(request, response) {
   var query = new Parse.Query('Song');
-  query.equalTo('href', request.object.get('href'));
-  query.equalTo('party_session', request.object.get('party_session'));
-  query.find({
-    success: function(res) {
-      if (res.length === 0) {
-        var defaults = {
-          up_votes: 1,
-          down_votes: 0,
-          played: false
-        };
-        Object.keys(defaults).forEach(function(key) {
-          if (typeof request.object.get(key) === 'undefined') {
-            request.object.set(key, defaults[key]);
-          }
-        });
-        response.success();
-      } else {
-        response.error('song already in playlist');
-      }
+  query.get(request.params.songId, {
+    success: function(song) {
+      song.set('played', true);
+      song.save(null);
+      response.success();
     },
     error: function() {
       response.error('song lookup failed');
     }
   });
+});
+
+
+Parse.Cloud.beforeSave('Song', function(request, response) {
+  if (request.object.id) {
+    response.success();
+  } else {
+    var query = new Parse.Query('Song');
+    query.equalTo('href', request.object.get('href'));
+    query.equalTo('party_session', request.object.get('party_session'));
+    query.find({
+      success: function(res) {
+        if (res.length === 0) {
+          var defaults = {
+            up_votes: 1,
+            down_votes: 0,
+            played: false
+          };
+          Object.keys(defaults).forEach(function(key) {
+            if (typeof request.object.get(key) === 'undefined') {
+              request.object.set(key, defaults[key]);
+            }
+          });
+          response.success();
+        } else {
+          response.error('song already in playlist');
+        }
+      },
+      error: function() {
+        response.error('song lookup failed');
+      }
+    });
+  }
 });
